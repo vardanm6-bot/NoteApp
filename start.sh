a0d57a25d6ca351c1c91194330ba820b8721e1f3
martirosyanv192-svg <martirosyanv192@gmail.com>
2026-07-30 14:05:55 -0700

    Fix dynamic PORT for Nginx

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+
+# Եթե PORT-ը սահմանված չէ, լռելյայն դնել 80
+PORT=${PORT:-80}
+
+# nginx.conf-ում PORT_NUMBER-ը փոխարինել իրական PORT-ով
+sed -i "s/PORT_NUMBER/$PORT/g" /etc/nginx/sites-available/default 2>/dev/null || sed -i "s/PORT_NUMBER/$PORT/g" /etc/nginx/conf.d/default.conf 2>/dev/null
+
+# Միացնել PHP-FPM-ը
 php-fpm -D
+
+# Միացնել Nginx-ը
 nginx -g 'daemon off;'
